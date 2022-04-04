ec14913b9357e6627d4efd16993db780bc7594d4
William Jordan <WilliamKatahdin@gmail.com>
2022-04-04 12:59:57 -0600

    added Classes

---
+++ Sprint1/Main.java
@@ -0,0 +1,8 @@
+package Sprint1;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ Sprint1/Recipe.java
@@ -0,0 +1,4 @@
+package Sprint1;
+
+public class Recipe {
+}
