28a3e201aa774d32f46bd8a967382d16352a08e7
Nicholas C <nicholascall6@gmail.com>
2022-04-05 20:22:37 -0600

    fixed old names in Ingredient class, added viewEditAMount() to Ingredient

--- Sprint1/Ingredient.java
+++ Sprint1/Ingredient.java
@@ -1,20 +1,59 @@
 package Sprint1;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
-public abstract class Ingredient implements Food {
+public abstract class Ingredient {
 
     private ArrayList<FoodUnits> units = new ArrayList<FoodUnits>();
     private double amount = 0.0F; // in grams
 
-    public Ingredient(float amountGrams)
+    private String name;
+
+    public Ingredient(String name, float amountGrams)
     {
-        
+        this.name = name;
+        setAmount(amountGrams);
     }
 
     public void addUnit(FoodUnits unit) {
         units.add(unit);
     }
+
+    /**
+     * set the amount using a specific unit
+     * @return
+     */
+    public void setAmount(double amount, int unitIndex) {
+        this.amount = units.get(unitIndex).toGrams(amount);
+    }
+
+    /**
+     * @return the amount in the specified units
+     */
+    public double getAmount(int unitIdx) {
+        return units.get(unitIdx).fromGrams(amount);
+    }
+
+    /**
+     * @return the amount in grams
+     */
+    public double getAmount() {
+        return amount;
+    }
+
+    /**
+     * @return set the amount using grams
+     */
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
 
     /**
      * Display the unit options for the user to choose from
@@ -25,11 +64,34 @@ public abstract class Ingredient implements Food {
         }
     }
 
-    public void setAmount(float amount) {
+    /**
+     * command line interface for the user to edit the amount
+     */
+    public void viewEditAmount() {
+        Scanner scanner = new Scanner(System.in);
+        int unitIdx = 0;
 
+
+        // first let user select the desired unit
+        if (units.size() > 1) {
+            
+            System.out.println("Select a unit to edit the amount");
+            viewAmountUnits();
+
+            unitIdx = scanner.nextInt();
+        }
+
+        // now let the user edit the amount
+        System.out.println("Input the " + getName() +
+         " amount in " + units.get(unitIdx).getUnits());
+
+        double amount = scanner.nextDouble();
+        setAmount(amount, unitIdx);
+
+        scanner.close();
     }
 
-
-    public double getAmount(int unitIdx);
+   // public void getNutrients(); // @TODO what does this return?
+   // public void getRestrictions(); // @TODO what does this return?
 
 }
