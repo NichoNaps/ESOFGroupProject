d9de53c19b6a61d5e3c7e530e70df0179330f402
Nicholas C <nicholascall6@gmail.com>
2022-04-05 21:29:32 -0600

    I remembered that math exists and that a cup is a measure of volume and a gram is a measure of mass so I'll be redoing the units with that in mind so you can specify density when needed.

---
+++ Units/Unit.java
@@ -0,0 +1,10 @@
+package Units;
+import java.util.Scanner;
+
+public interface Unit {
+
+    public double fromGrams(double amount);
+    public double toGrams(double amount);
+    public String getUnits();
+
+}
