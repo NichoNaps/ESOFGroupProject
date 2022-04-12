35fa8b080aec8eca62326e4982a5c726bd529afd
Amanda Faulconer <78063172+AmandaFaulconer@users.noreply.github.com>
2022-04-12 08:09:36 -0600

    Update WeeklyPlan.java

--- WeeklyPlan.java
+++ WeeklyPlan.java
@@ -1,4 +1,26 @@
 package Sprint1;
 
 public class WeeklyPlan {
+	
+	//print statement for user
+	System.out.println("Hello" + user.getName() + " here is your weekly meal plan");
+	
+	//objects for the daily meal list
+	DailyPlan Monday = new DailyPlan();
+	DailyPlan Tuesday = new DailyPlan();
+	DailyPlan Wednesday = new DailyPlan();
+	DailyPlan Thursday = new DailyPlan();
+	DailyPlan Friday = new DailyPlan();
+	DailyPlan Saturday = new DailyPlan();
+	DailyPlan Sunday = new DailyPlan();
+	
+	//print statement for the user to see all meals
+	System.out.println(Monday.createPlan() 
+						+ Tuesday.createPlan() 
+						+ Wednesday.createPlan() 
+						+ Thuesday.createPlan() 
+						+ Friday.createPlan() 
+						+ Saturday.createPlan()
+						+ Sunday.createPlan() 
+						);
 }
