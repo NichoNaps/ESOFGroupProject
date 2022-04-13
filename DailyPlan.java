a680c97c86f4be7610a8ba1279093342c848ea74
EthanSkelton9 <bigeskelly@gmail.com>
2022-04-12 18:52:36 -0600

    Committing DailyPlan

--- DailyPlan.java
+++ DailyPlan.java
@@ -1,5 +1,22 @@
 package Sprint1;
+import java.io.*;
+import java.util.*;
+public class DailyPlan extends {
 
-public class DailyPlan {
-    
+    public ArrayList<Recipe> createPlan(){
+        //start local variables
+        int iNumofMeals = 3; //number of meals for the day
+        ArrayList<Recipe> dailyPlan = new ArrayList<Recipe>(iNumofMeals); //array of recipes for day
+        //end local variables
+
+
+
+        for (int i = 1; i <= iNumofMeals; i++) {
+            Recipe recipe = new Recipe();
+            dailyPlan.add(recipe.getIngredients());
+        }
+
+        return dailyPlan;
+    }
+
 }
