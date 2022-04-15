abdf3e5600ea31b3b490da0bd7b1dac70646a9a0
Amanda Faulconer <78063172+AmandaFaulconer@users.noreply.github.com>
2022-04-15 11:17:33 -0600

    Update WeeklyPlan.java
    moved days to private and create a function for the week plan

--- WeeklyPlan.java
+++ WeeklyPlan.java
@@ -2,25 +2,32 @@ package ESOFGroupProject;
 
 public class WeeklyPlan {
 	
-	//print statement for user
-	System.out.println("Hello" + user.getName() + " here is your weekly meal plan");
+	private DailyPlan monday = new DailyPlan();
+	private DailyPlan tuesday = new DailyPlan();
+	private DailyPlan wednesday = new DailyPlan();
+	private DailyPlan thursday = new DailyPlan();
+	private DailyPlan friday = new DailyPlan();
+	private DailyPlan saturday = new DailyPlan();
+	private DailyPlan sunday = new DailyPlan();
 	
-	//objects for the daily meal list
-	DailyPlan Monday = new DailyPlan();
-	DailyPlan Tuesday = new DailyPlan();
-	DailyPlan Wednesday = new DailyPlan();
-	DailyPlan Thursday = new DailyPlan();
-	DailyPlan Friday = new DailyPlan();
-	DailyPlan Saturday = new DailyPlan();
-	DailyPlan Sunday = new DailyPlan();
 	
-	//print statement for the user to see all meals
-	System.out.println(Monday.createPlan() 
-						+ Tuesday.createPlan() 
-						+ Wednesday.createPlan() 
-						+ Thuesday.createPlan() 
-						+ Friday.createPlan() 
-						+ Saturday.createPlan()
-						+ Sunday.createPlan() 
-						);
+	public void weeklyPlan(){
+		//print statement for user
+		System.out.println("Hello" + user.getName() + " here is your weekly meal plan");
+		
+		
+		//print statement for the user to see all meals
+		System.out.println(getMonday(): createPlan
+		
+							+ Tuesday.createPlan() 
+							+ Wednesday.createPlan() 
+							+ Thuesday.createPlan() 
+							+ Friday.createPlan() 
+							+ Saturday.createPlan()
+							+ Sunday.createPlan() 
+							);
+			
+		
+	}
+	
 }
