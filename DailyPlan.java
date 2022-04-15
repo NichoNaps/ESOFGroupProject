e14915c07f090a94533a22a5204a1d377638f245
EthanSkelton9 <bigeskelly@gmail.com>
2022-04-15 09:44:10 -0600

    DailyPlan update

--- DailyPlan.java
+++ DailyPlan.java
@@ -3,22 +3,26 @@ package ESOFGroupProject;
 import java.io.*;
 import java.util.*;
 
-public class DailyPlan extends {
+public class DailyPlan {
+    //start varibale declaration
+    private static final int iNumofMeals = 3; //number of meals for the day
+    private ArrayList<Recipe> recipes = new ArrayList<Recipe>(iNumofMeals); //array of recipes for day
+    private int check = 0; //check if array has been populated
+    //end variable declaration
 
-    public ArrayList<Recipe> createPlan(){
-        //start local variables
-        int iNumofMeals = 3; //number of meals for the day
-        ArrayList<Recipe> dailyPlan = new ArrayList<Recipe>(iNumofMeals); //array of recipes for day
-        //end local variables
+    public ArrayList<Recipe> getRecipes(){ // returns an array of recipes
+        if(check == 0){  // If the array has not been populated then populate the array and set the check to 1
+            check = 1;
+            populateRecipes();   
+        }
+        return recipes;
+    }
 
-
-
+    private ArrayList<Recipe> populateRecipes(){ // populates the array with recipes
         for (int i = 1; i <= iNumofMeals; i++) {
             Recipe recipe = new Recipe();
-            dailyPlan.add(recipe.getIngredients());
+            recipes.add(recipe.getIngredients());
         }
-
-        return dailyPlan;
     }
 
 }
