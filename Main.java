4d33cd758f4bf391a1151f8762723b0ed27ba838
William Jordan <WilliamKatahdin@gmail.com>
2022-04-16 00:11:40 -0600

    Updated REPL
    changed form recursion to while loop

--- Main.java
+++ Main.java
@@ -17,20 +17,25 @@ public class Main {
 
     public void REPL(){
         Scanner scan = new Scanner(System.in);
+        while (true) {
         System.out.println("What do you want to do?");
         System.out.println(" 1. Look at Plan");
         System.out.println(" 2. View Grocery List");
         System.out.println(" 3. Update User Info");
         System.out.println(" X. Exit");
-
-        switch (scan.nextLine()){
-            case "1" -> viewPlan();
-            case "2" -> viewGroceryList();
-            case "3" -> updateUserInfo();
-            case "X", "x" -> System.out.println("Exit Complete");
-            default -> {
-                System.out.println("Invalid Input");
-                REPL();
+            switch (scan.nextLine()) {
+                case "1" -> viewPlan();
+                case "2" -> viewGroceryList();
+                case "3" -> {
+                    updateUserInfo();
+                    System.out.println("Update Complete\n");
+                }
+                case "X", "x" -> {
+                    System.out.println("Exit Complete");
+                    scan.close();
+                    return;
+                }
+                default -> System.out.println("Invalid Input");
             }
         }
     }
@@ -38,45 +43,47 @@ public class Main {
 
     private void viewPlan(){
         System.out.println(user.toString());
-        REPL();
     }
 
     private void viewGroceryList() {
         System.out.println(user.toString());
-        REPL();
     }
 
-    private void updateUserInfo(){
+    private void updateUserInfo() {
         System.out.println("What would you like to change?");
         System.out.println(" 1. Name");
         System.out.println(" 2. Age");
         System.out.println(" 3. Weight");
         System.out.println(" 4. Calorie Goal");
         System.out.println(" X. Exit");
-        Scanner scan = new Scanner(System.in);
-        switch (scan.nextLine()){
-            case "1" -> {
-                System.out.println("Enter new name:");
-                user.setName(scan.nextLine());
-            }
-            case "2" -> {
-                System.out.println("Enter new age:");
-                user.setAge(scan.nextInt());
-            }
-            case "3" -> {
-                System.out.println("Enter new weight:");
-                user.setWeight(scan.nextInt());
-            }
-            case "4" -> {
-                System.out.println("Enter new calorie goal:");
-                user.setDailyCalories(scan.nextInt());
-            }
-            case "x", "X" -> {}
-            default -> {
-                System.out.println("Invalid Input");
-                updateUserInfo();
+        while (true) {
+            Scanner scan = new Scanner(System.in);
+            switch (scan.nextLine()) {
+                case "1" -> {
+                    System.out.println("Enter new name:");
+                    user.setName(scan.nextLine());
+                    return;
+                }
+                case "2" -> {
+                    System.out.println("Enter new age:");
+                    user.setAge(scan.nextInt());
+                    return;
+                }
+                case "3" -> {
+                    System.out.println("Enter new weight:");
+                    user.setWeight(scan.nextInt());
+                    return;
+                }
+                case "4" -> {
+                    System.out.println("Enter new calorie goal:");
+                    user.setDailyCalories(scan.nextInt());
+                    return;
+                }
+                case "x", "X" -> {
+                    return;
+                }
+                default -> System.out.println("Invalid Input");
             }
         }
-        REPL();
     }
 }
